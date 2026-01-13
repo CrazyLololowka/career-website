4e4c79873eeecd0735a11f2a0a68dad9049bdf6d
CrazyLololowka <romakurochka52@gmail.com>
2026-01-13 16:10:07 +0000

    added something xd

--- app.py
+++ app.py
@@ -2,9 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+OFFERS =[
+{
+  "id" : 1,
+  "title" : "Pinky",
+  "price" : "100000 Rubles"
+  },
+{ 
+  "id" : 2,
+  "title" : "Rainbow",
+  "price" : "104000 Rubles"
+} 
+]
+
 @app.route('/') 
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', offers=OFFERS)
   
 print(__name__)  
 if __name__ == '__main__':
